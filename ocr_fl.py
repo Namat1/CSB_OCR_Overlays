bb052cc6fb08a2ce7199f10c08d49ab363f224ad
Martin <mcarstensen79@gmail.com>
2025-01-08 19:31:32 +0100

    Update ocr_fl.py

--- ocr_fl.py
+++ ocr_fl.py
@@ -148,7 +148,7 @@ if uploaded_pdf and uploaded_excel:
             progress_bar.progress(75)
 
             # Overlays hinzufügen und Namen ins PDF schreiben
-            output_pdf = add_overlays_with_text_on_top(uploaded_pdf, page_name_map, name_x=276, name_y=785)
+            output_pdf = add_overlays_with_text_on_top(uploaded_pdf, page_name_map, name_x=280, name_y=785)
             time.sleep(1)  # Simuliere Arbeit
             progress_bar.progress(100)
 
